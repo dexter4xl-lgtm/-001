2244853df30378076ff12b6859962d802722db39
``dexter4xl-lgtm`` <``dexter4xl@gmail.com``>
2025-11-04 12:10:58 -0300

    primeiro commit

--- jvscript/script.js
+++ jvscript/script.js
@@ -1,6 +1,18 @@
-const toggle = document.getElementById('menu-toggle');
-const menu = document.querySelector('.menu');
+// ======= MENU RESPONSIVO =======
 
+// Seleciona o botão e o menu
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+// Abre e fecha o menu quando clica no botão ☰
 toggle.addEventListener('click', () => {
   menu.classList.toggle('show');
+});
+
+// Fecha o menu quando clica em um link
+const links = document.querySelectorAll('.menu-link');
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('show');
+  });
 });
